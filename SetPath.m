838c306ca1c3f2e1818d773d1a0adde47e5caeba
Isaac Rodríguez-Padilla <59874126+irpadilla@users.noreply.github.com>
2022-08-04 22:03:01 -0500

    Update SetPath.m

--- SetPath.m
+++ SetPath.m
@@ -10,7 +10,8 @@
 
 % Add relevant paths to Matlab
 Path = '/home/irpadilla/Bureau/ImageStabilization-Toolbox/'; % main path where the image stabilization toolbox is located
-PathImages   = [Path,'demoClip2/'         ];   % path where the frames are stored. If the frames are stored in different subfolders, please indicate the top-level folder
+PathImages   = [Path,'Frames/'                  ]; % path where the frames are stored. If the frames are stored in different subfolders, please indicate the top-level folder
+PathStabilizedImages = [Path,'StabilizedFrames/']; % path where the stabilized frames will be stored 
 ExtImages    = ['jpg'                     ];   % extension of the images (jpg, png)
 PathMatfiles = [Path,'Matfiles/'          ];   % path where the matlab files are or will be stored
 PathFigures  = [Path,'Figures/'           ];   % path where the figures are or will be stored
